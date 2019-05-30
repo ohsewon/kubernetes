ba762f82041b7409f981d6a5ad01af3a2eda16b5
nolancon <conor.nolan@intel.com>
2019-05-30 14:44:14 +0100

    Fix boilerplate and uncomment line

--- pkg/kubelet/cm/cpumanager/topology_hints.go
+++ pkg/kubelet/cm/cpumanager/topology_hints.go
@@ -1,9 +1,12 @@
 /*
 Copyright 2019 The Kubernetes Authors.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -128,7 +131,7 @@ func getPreferred(cpuHints []topologymanager.TopologyHint, requested int, CPUsPe
 			cpuHints[r].Preferred = false
 			klog.Infof("Set Preferred to false: (there are more preferable hints) %v", cpuHints[r])
 		} else if cpuHints[r].SocketAffinity.Count() == bestHint && requested >= CPUsPerSocket {
-			//cpuHints[r].Preferred = false
+			cpuHints[r].Preferred = false
 			klog.Infof("Set Preferred to false (could never be satisfied on one socket): %v", cpuHints[r])
 		}
 	}
